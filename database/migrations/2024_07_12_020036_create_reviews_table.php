4bf3d3c6bbe68b34bfaeabdc8e935029aedc2078
kyishon <haruta685@gmail.com>
2024-07-12 12:48:35 +0630

    san-kyi-tar-par

--- database/migrations/2024_07_12_020036_create_reviews_table.php
+++ database/migrations/2024_07_12_020036_create_reviews_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->integer('rating');
             $table->text('comments')->nullable();
             $table->timestamps();
-            //san kyi tar par
 
         });
 
